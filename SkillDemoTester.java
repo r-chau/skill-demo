de03298770cf8d742554868724027a80445a47a8
r-chau <97642931+r-chau@users.noreply.github.com>
2022-03-18 12:27:53 -0700

    Update SkillDemoTester.java
    fix test

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test
     public void multiply() {
-        assertEquals(9, SkillDemoOne.mult(2, 3)); //this test should fail
+        assertEquals(9, SkillDemoOne.mult(3, 3)); //this test should fail
     }
 }
